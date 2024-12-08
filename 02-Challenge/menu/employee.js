2e05433ad3fa46fef7d9cd51058019e8da0d0396
Baileigh Remy <baileigh.remy@gmail.com>
2024-12-08 15:18:06 -0600

    Added Employee class

--- 02-Challenge/menu/employee.js
+++ 02-Challenge/menu/employee.js
@@ -1,1 +1,27 @@
 const db = require("../db/connection");
+
+class Employee {
+    constructor(id, first_name, last_name, role_id, manager_id) {
+        (this.id = id), 
+        (this.first_name = first_name),
+        (this.last_name = last_name),
+        (this.role_id = role_id),
+        (this.manager_id = manager_id)
+    }
+    getAll() {
+        const sql = `SELECT * FROM role`;
+        return db
+        .promise()
+        .query(sql)
+        .then(([rows]) => {
+        return rows;
+    });
+}
+
+addEmployee() {
+    const sql = `INSERT INTO role (first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4)`
+    return db.promise().query(sql, [this.first_name, this.last_name, this.role_id, this.manager_id]);
+}
+}
+
+module.exports = Employee;
