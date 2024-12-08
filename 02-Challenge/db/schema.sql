7133e485b73d2b5e954e34d8dac02680af58fa39
Baileigh Remy <baileigh.remy@gmail.com>
2024-12-08 14:00:55 -0600

    Added schema tables

---
+++ 02-Challenge/db/schema.sql
@@ -0,0 +1,26 @@
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS employee;
+
+CREATE TABLE 
+    department (
+        id INTEGER AUTO_INCREMENT SERIAL PRIMARY KEY,
+        name VARCHAR(30) UNIQUE NOT NULL
+    );
+
+CREATE TABLE
+    role (
+        id AUTO_INCREMENT SERIAL PRIMARY KEY,
+        title VARCHAR(30) UNIQUE NOT NULL,
+        salary DECIMAL NOT NULL,
+        department_id: INTEGER NOT NULL
+    );
+
+CREATE TABLE 
+    employee (
+        id AUTO_INCREMENT SERIAL PRIMARY KEY,
+        first_name VARCHAR(30) NOT NULL,
+        LAST_NAME VARCHAR(30) NOT NULL, 
+        role_id INTEGER NOT NULL,
+        manager_id INTEGER,
+    )
